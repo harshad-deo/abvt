d6d9cdc0c3c73edd312b73402d626f360716b327
Harshad Deo <subterranean.hominid@gmail.com>
2020-05-23 19:56:06 +0530

    add warp, tokio

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,14 @@ extern crate pretty_env_logger;
 #[macro_use]
 extern crate log;
 
-fn main() {
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
     pretty_env_logger::init();
-    info!("Hello, world!");
+    info!("Initializing ABVT");
+
+    let hello = warp::path!("hello" / String).map(|name| format!("Hello {}!", name));
+
+    warp::serve(hello).run(([0, 0, 0, 0], 8080)).await;
 }
